ad1aba0f37f45f5fb16ad0be602c993b85c3dbaf
Dmitri Trofimov <dmitri_trofimov@trimble.com>
2025-06-24 18:41:36 +0300

    made palindrome generator even faster

--- node-js/type-script/src/math/palindromic_numbers.ts
+++ node-js/type-script/src/math/palindromic_numbers.ts
@@ -1,34 +1,4 @@
-export function* generatePalindromicNumbers(base = 10) {
-  if (base < 2 || !Number.isInteger(base)) {
-    throw new Error('Base must be an integer greater than or equal to 2.');
-  }
-
-  yield 0;
-
-  for (let length = 1; ; length++) {
-    const halfLength = Math.ceil(length / 2);
-    const firstHalfStart = halfLength === 1 ? 1 : base ** (halfLength - 1);
-    const firstHalfEnd = base ** halfLength;
-
-    for (let firstHalf = firstHalfStart; firstHalf < firstHalfEnd; firstHalf++) {
-      let palindrome = firstHalf;
-      let temp = firstHalf;
-
-      if (length % 2 === 1) {
-        temp = Math.floor(temp / base);
-      }
-
-      while (temp > 0) {
-        const digit = temp % base;
-        palindrome = palindrome * base + digit;
-        temp = Math.floor(temp / base);
-      }
-      yield palindrome;
-    }
-  }
-}
-
-export function* generatePalindromicNumbers2(base = 10) {
+export function* generatePalindromicNumbers(base = 10): Generator<number> {
   if (base < 2 || !Number.isInteger(base)) {
     throw new Error('Base must be an integer greater than or equal to 2.');
   }
@@ -40,7 +10,6 @@ export function* generatePalindromicNumbers2(base = 10) {
   let isOddLength = true;
 
   while (true) {
-    // increment
     let carry = 1;
 
     for (let i = 0; i < digits.length; i++) {
@@ -69,14 +38,13 @@ export function* generatePalindromicNumbers2(base = 10) {
       isOddLength = !isOddLength;
     }
 
-    // reconstruct palindrome
     let palindrome = 0;
 
-    for (let i = 0; i < digits.length; i++) {
+    for (let i = digits.length - 1; i >= 0; i--) {
       palindrome = palindrome * base + digits[i];
     }
 
-    for (let i = digits.length - (isOddLength ? 1 : 2); i >= 0; i--) {
+    for (let i = isOddLength ? 1 : 0; i < digits.length; i++) {
       palindrome = palindrome * base + digits[i];
     }
 
